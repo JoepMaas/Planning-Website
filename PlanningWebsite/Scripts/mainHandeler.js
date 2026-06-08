edaae67484d3f6fdfd21c591a09bf04978264ec2
Joep Maas <j.maas1@sintlucasedu.nl>
2026-06-08 23:33:44 +0200

    Made the main framework for the planning site.

---
+++ PlanningWebsite/Scripts/mainHandeler.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+});
+
+const welcomeText = document.getElementById("WelcomeTitle");
+
+let user = "Joep";
+
+
+welcomeText.innerText = "Good to see you again, " + user + "!";
+
