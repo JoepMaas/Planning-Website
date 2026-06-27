7b4e7fab3fdd3676611068694ea35c5e028fcd00
Joep Maas <j.maas1@sintlucasedu.nl>
2026-06-28 00:38:35 +0200

    Added a json and a loading function for the json

--- planningHandeler.js
+++ planningHandeler.js
@@ -1,45 +1,51 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    loaded()
+    loaded();
 });
 
-function loaded () {
-    
+function loaded() {
+    loadData();
 }
 
 let maxCards = 10;
 let addedBoard = 0;
 let boardN = 0;
-let addedCard = [0,0,0,0,0];
-let Opened = [false,false,false,false,false];
+let addedCard = [0, 0, 0, 0, 0];
+let Opened = [false, false, false, false, false];
+
+async function loadData() {
+    const response = await fetch("BoardSaving.json");
+    const data = await response.json();
+
+    LoadBoards(data);
+}
 
 function AddBoard() {
     if (addedBoard < 4) {
         addedBoard++;
         ToggledEditMode = true;
-        
+
         let board = document.getElementsByClassName("placeholderBoard1")[0];
         let addBoard = document.getElementsByClassName("AddBoard");
-        
+
         let copy = board.cloneNode(true);
-        let copyBoard = addBoard[0].cloneNode(true);     
-        
+        let copyBoard = addBoard[0].cloneNode(true);
+
         copy.classList.remove("placeholderBoard1");
         copy.classList.add("board" + (addedBoard + 1));
 
-        const addCardBtn = copy.querySelector('.addCard');
-        addCardBtn.setAttribute('onclick', `AddCard(${addedBoard})`);
-        
-        const EditBoardBtn = copy.querySelector('.EditBoard');
-        EditBoardBtn.setAttribute('onclick', `EditBoard(${addedBoard}, false)`);
-    
+        const addCardBtn = copy.querySelector(".addCard");
+        addCardBtn.setAttribute("onclick", `AddCard(${addedBoard})`);
+
+        const EditBoardBtn = copy.querySelector(".EditBoard");
+        EditBoardBtn.setAttribute("onclick", `EditBoard(${addedBoard}, false)`);
+
         document.getElementsByClassName("BoardsContainer")[0].appendChild(copy);
         document.getElementsByClassName("BoardsContainer")[0].appendChild(copyBoard);
-    
+
         addBoard[0].remove();
         if (addedBoard == 4) {
             addBoard[0].style.display = "none";
         }
-
     }
 }
 
@@ -50,32 +56,32 @@ function AddCard(boardNumber) {
     let copyI = item.cloneNode(true);
     let copyC = addCard[boardNumber + 1].cloneNode(true);
 
-    let board = document.getElementsByClassName("board" + (boardNumber + 1))[0]; 
+    let board = document.getElementsByClassName("board" + (boardNumber + 1))[0];
     board.appendChild(copyI);
     board.appendChild(copyC);
-    
+
     board.scrollTop = board.scrollHeight;
-    
+
     addCard[boardNumber + 1].remove();
-    
+
     addedCard.splice(boardNumber, 1, addedCard[boardNumber] + 1);
-    
+
     //hiding: + add card
-    document.getElementsByClassName("addCard")[(boardNumber + 1)].style.display = (addedCard[boardNumber] == maxCards) ? "none" : "block";
-    copyI.getElementsByClassName("garbageIcon")[0].style.display = (Opened[boardNumber]) ? "block" : "none"; 
+    document.getElementsByClassName("addCard")[boardNumber + 1].style.display = addedCard[boardNumber] == maxCards ? "none" : "block";
+    copyI.getElementsByClassName("garbageIcon")[0].style.display = Opened[boardNumber] ? "block" : "none";
 }
 
-function RemoveCard(btn){
-    ((btn.parentNode).parentNode).removeChild(btn.parentNode)
+function RemoveCard(btn) {
+    btn.parentNode.parentNode.removeChild(btn.parentNode);
 
-    addedCard.splice(boardN, 1, (addedCard[boardN] -1));
+    addedCard.splice(boardN, 1, addedCard[boardN] - 1);
 
     if (addedCard[boardN] < 10) {
-        document.getElementsByClassName("addCard")[(boardN + 1)].style.display = "block";
+        document.getElementsByClassName("addCard")[boardN + 1].style.display =
+            "block";
     }
 
-    if (addedCard[boardN] == 0)
-    {
+    if (addedCard[boardN] == 0) {
         EditBoard(boardN, true);
     }
 }
@@ -86,8 +92,8 @@ function EditBoard(boardNumber, bool) {
             if (i !== boardNumber) {
                 Opened[i] = false;
                 let otherBoard = document.getElementsByClassName("board" + (i + 1))[0];
-                if (otherBoard) { 
-                    otherBoard.querySelectorAll('.garbageIcon').forEach(item => {
+                if (otherBoard) {
+                    otherBoard.querySelectorAll(".garbageIcon").forEach((item) => {
                         item.style.display = "none";
                     });
                 }
@@ -96,11 +102,39 @@ function EditBoard(boardNumber, bool) {
 
         Opened[boardNumber] = !Opened[boardNumber];
         let board = document.getElementsByClassName("board" + (boardNumber + 1))[0];
-        let items = board.querySelectorAll('.garbageIcon');
+        let items = board.querySelectorAll(".garbageIcon");
         boardN = boardNumber;
 
-        items.forEach(item => {
+        items.forEach((item) => {
             item.style.display = Opened[boardNumber] ? "block" : "none";
         });
     }
+}
+
+function LoadBoards(data) {
+    data.boards.forEach((boardData, index) => {
+        AddBoard();
+
+        let board = document.getElementsByClassName("board" + (index + 1))[0];
+
+        board.querySelector(".titleB").value = boardData.boardName;
+
+        let firstCard = board.querySelector(".ItemB");
+        if (firstCard) {
+            firstCard.remove();
+            addedCard[index]--;
+        }
+
+        boardData.items.forEach((item) => {
+            AddCard(index);
+            let cards = board.querySelectorAll(".ItemB");
+            let lastCard = cards[cards.length - 1];
+            lastCard.querySelector(".TitleItem").value = item.itemName;
+            lastCard.querySelector(".TextItem").value = item.textDescription;
+        });
+    });
+}
+
+function UpdateData() {
+    
 }
